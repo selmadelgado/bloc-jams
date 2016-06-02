2562cdbbb50f39cbd17870294ece48cefed09bc7
irasema <selmadelgado@gmail.com>
2016-06-02 14:18:28 -0500

    Checkpoint-29-jQuery-Landing-Page Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,38 +1,30 @@
-// Select all elements with the class name point
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });   
+     };
+     
 
-
-//Actions to perform on each element in the revealPoint nodeList
-var revealPoint = function(point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";
-};
+    $.each($('.point'), revealPoint);
+ };
  
 
-//Pass-in the array called PointsArray
-var animatePoints = function(points) {
-   forEach(points, revealPoint);
-};
 
-
-//Execute animatePoints function when window finishes loading
- window.onload = function() {
-    // Auto animate the points on a tall screen where scrolling can't trigger the animation
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+$(window).load(function() {
+    if ($(window).height() > 950) {
+        animatePoints();
      }
-    
-     //Execute animatePoints when client scrolls selling points into view
-     window.addEventListener("scroll", function(event) { 
-         if (pointsArray[0].getBoundingClientRect().top <= 500) {
-             animatePoints(pointsArray);
+     
+
+var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+     
+ 
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+           animatePoints();   
          }
- });
- }
- 
- 
- 
+     });
 
-
++});
