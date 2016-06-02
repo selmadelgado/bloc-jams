f890885759caeccbc952171801260695d62ab8fe
irasema <selmadelgado@gmail.com>
2016-06-02 15:42:49 -0500

    Checkpoint 30 jQuery Collection

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+    var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -13,18 +14,20 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>'
    ;
-
-
-window.onload = function() {
-    
-    //1. Select element with album-covers class name
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
   
-    //2. Assign empty string to clear its content
-     collectionContainer.innerHTML = '';
- 
-     //3. Create loop to add contents of collectionItemTemplate
-     for (var i = 0; i < 8; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+    return $(template);
+};
+
+
+
+$(window).load(function() {
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
+
+     for (var i = 0; i < 12; i++) {
+
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
      }
- }
+
+}); 
