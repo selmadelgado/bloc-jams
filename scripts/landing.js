50b2b2db473fcc6ec8f556b174b1f8f0c91dbd79
irasema <selmadelgado@gmail.com>
2016-06-02 14:42:42 -0500

    Checkpoint 29 jQuery Landing Page Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,29 @@
 var animatePoints = function() {
+
     var revealPoint = function() {
-        $(this).css({
-            opacity: 1,
-            transform: 'scaleX(1) translateY(0)'
-        });   
-     };
-     
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+    };
 
     $.each($('.point'), revealPoint);
- };
- 
 
+};
 
 $(window).load(function() {
-    if ($(window).height() > 950) {
-        animatePoints();
-     }
-     
 
-var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
-     
- 
-    $(window).scroll(function(event) {
-        if ($(window).scrollTop() >= scrollDistance) {
-           animatePoints();   
-         }
-     });
+  //Auto animate the points on a tall screen where scrolling can't trigger  animation
+  if ($(window).height() > 950) {
+           animatePoints();
+  }
 
-+});
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+  $(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+    }
+  });
+
+});
